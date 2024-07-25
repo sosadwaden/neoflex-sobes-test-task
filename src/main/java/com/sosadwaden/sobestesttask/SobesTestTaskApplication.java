0d1eef3e6a4885b3ae9aaaf62f5cf092dd1a37f1
sosadwaden <wadenjap@gmail.com>
2024-07-25 12:33:13 +0300

    init Spring Boot app

---
+++ src/main/java/com/sosadwaden/sobestesttask/SobesTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sosadwaden.sobestesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobesTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SobesTestTaskApplication.class, args);
+    }
+
+}
